e36f1b31c5e201d806346834117499bcc5b7d1fa
sapir talker <sapirtalk@gmail.com>
2022-06-20 14:29:19 +0300

    changed email to post

--- routes/emailHandler.js
+++ routes/emailHandler.js
@@ -2,6 +2,6 @@ const express = require('express');
 const emailHandler = require('../controllers/emailHandler-controller');
 const router = express.Router();
 
-router.get('/', emailHandler.sendEmail);
+router.post('/', emailHandler.sendEmail);
 
 module.exports = router;
